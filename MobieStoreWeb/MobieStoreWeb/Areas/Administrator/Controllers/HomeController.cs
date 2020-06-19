a2d8bb03994bfbeff540e0dedd668d3702f1a599
congchinh262 <39187950+congchinh262@users.noreply.github.com>
2020-06-19 20:01:49 +0700

    Update Admin Dashboard, Filter Order

--- MobieStoreWeb/MobieStoreWeb/Areas/Administrator/Controllers/HomeController.cs
+++ MobieStoreWeb/MobieStoreWeb/Areas/Administrator/Controllers/HomeController.cs
@@ -4,8 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using MobieStoreWeb.Areas.Administrator.Models;
 using MobieStoreWeb.Data;
+using MobieStoreWeb.Helpers;
+using MobieStoreWeb.Models;
 
 namespace MobieStoreWeb.Areas.Administrator.Controllers
 {
@@ -19,28 +23,64 @@ namespace MobieStoreWeb.Areas.Administrator.Controllers
         {
             _context = context;
         }
+
         public IActionResult Index()
         {
+            ViewBag.PaymentStatuses = SelectListHelper.GetEnumrableList<PaymentStatus>().Where(e => e.Value != PaymentStatus.Unpaid.ToString() && e.Value != PaymentStatus.Voided.ToString()).ToList();
             return View();
         }
 
         // GET: /Administrator/Home/GetRevenueMonths
-        public IActionResult GetRevenueMonths()
+        public async Task<IActionResult> GetRevenueMonths(DateTime? toDate,IEnumerable<PaymentStatus> paymentStatuses, bool withCurrentMonth = true, int prevMonthCount = 12)
         {
-            var toDate = new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month + 1, 1); //VD: 2020/6/1
-            var fromDate = toDate.AddMonths(-12).AddDays(-1); // 2019/5/31
-            var orders = _context.Orders.Where(o => o.OrderDate > fromDate && o.OrderDate < toDate).Include(o => o.OrderDetails);
-            var groupingMonths = orders.GroupBy(o => o.OrderDate.Month)
-                                .Select(grouping => new
-                                {
-                                    Month = grouping.Key,
-                                    Orders = grouping.Select(o => o)
-                                });
-            var revenueMonths = groupingMonths.Select(grouping => new
+            if (prevMonthCount > 24)
             {
-                grouping.Month,
-                Revenue = grouping.Orders.Sum(o => o.OrderDetails.Sum(od => od.Price * od.Quantity))
-            });
+                prevMonthCount = 24;
+            }
+            if (!toDate.HasValue)
+            {
+                toDate = DateTime.Today;
+            }
+            var fromDate = new DateTime(toDate.Value.Year, toDate.Value.Month, 1).AddMonths(withCurrentMonth ? -prevMonthCount + 1 : -prevMonthCount);
+            var filterOrders = _context.Orders.Where(o => o.OrderDate >= fromDate && o.Status != OrderStatus.Cancelled);
+            if(paymentStatuses?.Count() > 0)
+            {
+                filterOrders = filterOrders.Where(o => paymentStatuses.Contains(o.PaymentStatus));
+            }
+            var filterOrdersWithTotal = await filterOrders
+                .Include(o => o.OrderDetails)
+                .Select(o => new
+                {
+                    Id = o.Id,
+                    OrderDate = o.OrderDate,
+                    Total = o.OrderDetails.Sum(od => od.Price * od.Quantity)
+                })
+                .OrderBy(o => o.OrderDate)
+                .AsNoTracking()
+                .ToListAsync();
+
+            var groupByMonthAndYear = filterOrdersWithTotal
+                .GroupBy(o => new { o.OrderDate.Month, o.OrderDate.Year })
+                .Select(g => new RevenueMonth
+                {
+                    Month = g.Key.Month,
+                    Year = g.Key.Year,
+                    Revenue = g.Sum(o => o.Total)
+                });
+
+            var revenueMonths = new List<RevenueMonth>();
+
+            for (int i = 0; i < prevMonthCount; i++)
+            {
+                var newDateTime = fromDate.AddMonths(i);
+                revenueMonths.Add(new RevenueMonth
+                {
+                    Month = newDateTime.Month,
+                    Year = newDateTime.Year,
+                    Revenue = groupByMonthAndYear.FirstOrDefault(g => g.Month == newDateTime.Month && g.Year == newDateTime.Year)?.Revenue ?? 0
+                });
+            }
+
             return Ok(revenueMonths);
         }
     }
--- MobieStoreWeb/MobieStoreWeb/Areas/Administrator/Controllers/OrdersController.cs
+++ MobieStoreWeb/MobieStoreWeb/Areas/Administrator/Controllers/OrdersController.cs
@@ -5,8 +5,10 @@ using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MobieStoreWeb.Data;
+using MobieStoreWeb.Helpers;
 using MobieStoreWeb.Models;
 
 namespace MobieStoreWeb.Areas.Administrator.Controllers
@@ -21,23 +23,40 @@ namespace MobieStoreWeb.Areas.Administrator.Controllers
         {
             _context = context;
         }
-        public IActionResult Index(string searchString)
+
+        public IActionResult Index(string searchName, OrderStatus? status, PaymentMethod? paymentMethod, PaymentStatus? paymentStatus, DeliveryOption? deliveryOption)
         {
-           
-            ViewData["CurrentFilter"] = searchString;
-            IEnumerable<Order> list = (from o in _context.Orders select o);
-            var listOrderStatus = (from o
-                               in _context.Orders
-                                   select o.PaymentMethod).Distinct();
-            ViewBag.listOrderStatus = listOrderStatus;
-            if (!String.IsNullOrEmpty(searchString))
+
+            var orders = _context.Orders.AsQueryable();
+
+            if (status.HasValue)
             {
-                list = list.Where(o => o.DeliveryOption.ToString().Contains(searchString)    
-                                   || o.Status.ToString().Contains(searchString)
-                                   || o.PaymentMethod.ToString().Contains(searchString)
-                                   || o.PaymentStatus.ToString().Contains(searchString));
+                orders = orders.Where(o => o.Status == status);
             }
-            return View(list);
+            if (paymentMethod.HasValue)
+            {
+                orders = orders.Where(o => o.PaymentMethod == paymentMethod);
+            }
+            if (paymentStatus.HasValue)
+            {
+                orders = orders.Where(o => o.PaymentStatus == paymentStatus);
+            }
+            if (deliveryOption.HasValue)
+            {
+                orders = orders.Where(o => o.DeliveryOption == deliveryOption);
+            }
+            if (!string.IsNullOrWhiteSpace(searchName))
+            {
+                orders = orders.Where(o => o.BillingName.Contains(searchName));
+            }
+
+           
+            ViewBag.Status = new SelectList(SelectListHelper.GetEnumrableList<OrderStatus>(), "Value", "Text", status);
+            ViewBag.paymentMethod = new SelectList(SelectListHelper.GetEnumrableList<PaymentMethod>(), "Value", "Text", paymentMethod);
+            ViewBag.PaymentStatus = new SelectList(SelectListHelper.GetEnumrableList<PaymentStatus>(), "Value", "Text", paymentStatus);
+            ViewBag.DeliveryOption = new SelectList(SelectListHelper.GetEnumrableList<DeliveryOption>(), "Value", "Text", deliveryOption);
+            ViewBag.SearchName = searchName;
+            return View(orders);
         }
 
         [Authorize(Roles = "Admin")]
@@ -56,8 +75,8 @@ namespace MobieStoreWeb.Areas.Administrator.Controllers
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        [Authorize(Roles ="Admin")]
-        public async Task<IActionResult> Edit(int? id , [Bind("Id,Name")] Order order)
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Edit(int? id, [Bind("Id,Name")] Order order)
         {
             if (id != order.Id)
             {
